81e05aedafb5de5ad637505bab7a4e6ff86b66d2
kaviram2407 <91118091+kaviram2407@users.noreply.github.com>
2023-06-07 17:02:02 +0530

    Add files via upload

---
+++ diff_typeinhash.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.HashSet;
+
+
+public class diff_typeinhash {
+    public static void main(String[] args){
+        HashSet a=new HashSet();
+        a.add("kaviram");
+        a.add(16);
+        a.add("sudharajanainar");
+        a.add(24);
+        a.add(75.00);
+        a.add('k');
+        System.out.println(a);
+    }
+}
